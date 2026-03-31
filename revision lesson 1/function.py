989ee7196ec0b9be82a120ecab1ce51f9c7ed285
notagpra-lgtm <notagpra@gmail.com>
2026-03-31 12:55:15 +0200

    Add files via upload

---
+++ revision lesson 1/function.py
@@ -0,0 +1,17 @@
+# def greet():
+#     #print("Hello how are you",name)
+#     nami=input("What is your name?")
+#     print("Hello how are you",nami,"?")
+# greet()
+# greet()  
+
+# def add(num1,num2):
+#     result=num1+num2
+#     print(result)
+# add(24,6546)
+# add(31,43)
+
+def are(len,hig):
+    ans=len*hig
+    print(ans)
+are(21,65)
